6df72c68bc0e77e34cb0e14f1b90192b485d58ae
roeikimchi <101129927+roeikimchi@users.noreply.github.com>
2022-04-06 22:14:04 +0300

    isempty

--- Adv_C_Exe3/Main.c
+++ Adv_C_Exe3/Main.c
@@ -4,5 +4,12 @@
 
 void main()
 {
-	// add your code here
+	Stack s1;
+	initStack(&s1);
+	char c1 = 'a';
+	int num = isEmptyStack(&s1);
+	push(&s1,c1);
+	char c2 = pop(&s1);
+	num = isEmptyStack(&s1);
+	destroyStack(&s1);
 }
--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -1,31 +1,50 @@
 #include "Stack.h"
+#include <stdio.h>
 
 /***************** Stack ADT Implementation *****************/
 
 void initStack(Stack* s)
 {
-	// add your code here
+	s = (Stack*)malloc(sizeof(Stack));// Allocate memory for ADT 
+	if (s == NULL) {
+		printf("initstack: memory allocation faild\n");
+		return;
+	}
+	s->head = NULL;
 }
 
 void destroyStack(Stack* s)
 {
-	// add your code here
+	free(s); 
 }
 
 void push(Stack* s, char data)
 {
-	// add your code here
+	charNode* newnode = (charNode*)malloc(sizeof(charNode));
+	if (newnode == NULL) {
+		printf("push: memory allocation faild\n");
+		return;
+	}
+	newnode->data = data;
+	s->head = addToHead(s->head, newnode);
 }
 
 char pop(Stack* s)
 {
-	// add your code here
+	char tav = s->head->data;
+	removeItem(&(s->head));
+	return tav;
+	
 }
 
 
 int isEmptyStack(const Stack* s)
 {
-	// add your code here
+	if (s->head == NULL) {
+		return 1;
+	}
+	return 0; 
+
 }
 
 /*************** Functions using stacks - Implementation/definition **************************/
@@ -43,4 +62,16 @@ int isPalindrome(Stack* s)
 void rotateStack(Stack* s, int n)
 {
 	// add your code here
+}
+
+charNode* addToHead(charNode* head, charNode* toadd) {
+	toadd->next = head;
+	head = toadd;
+	return head;
+}
+
+void removeItem(charNode** head) {
+	charNode* tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
 }
